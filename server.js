5485e5499d31cdef08f62484eaae938425787f25
alibasit78 <alibasit78@gmail.com>
2017-08-09 23:58:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function (req, res) {
+  res.sendFile("Article one content");
+});
+
+app.get('/article-two', function (req, res) {
+  res.sendFile("Article two content");
+});
+
+app.get('/article-three', function (req, res) {
+  res.sendFile("Article three content");
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
