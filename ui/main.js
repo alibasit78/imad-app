0d8f0720011e5b06f625246c04f4f6d07a099fe7
alibasit78 <alibasit78@gmail.com>
2017-08-25 21:02:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,21 +6,14 @@ button.onclick=function(){
 	var request = new XMLHttpRequest();
 
 	//capture the response and store it in a variable
-	request.onreadystatechange=function(){
-	    			//request.log('what is happening');
+	request.onreadystate=function(){
 		if(request.readyState===XMLHttpRequest.DONE){
 			//take some action
-
 			if(request.status===200){
-			    	//console.log('hello');
 				var counter=request.responseText;
-			
 				var span=document.getElementById('count');
 				span.innerHTML=counter.toString(); 
-				
 			}
-			
-		
 		}
 	}; 
 	//Make the request
@@ -28,3 +21,22 @@ button.onclick=function(){
 	request.send(null);
 
 };
+
+//submit name
+var nameInput=document.getElementById("name");
+var name=nameInput.value;
+var submit=document.getElementById("submit_btn");
+submit.onclick()=function(){
+	//make a request to the server and send the name
+
+	//capture a list of names and render it as a list
+	var names=['name1','name2','name3','name4'];
+	var list='';
+	for(var i=0;i<names.length;i++)
+	{
+		list+='<li>' + names[i] + '</li>';
+	}
+	var ul=document.getElementById('namelist');
+	ul.innerHTML=list;
+}; 
+
