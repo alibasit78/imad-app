98703bb34c172c6465c91d982f59ce12f213fcd1
alibasit78 <alibasit78@gmail.com>
2017-09-03 12:20:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,28 +1,6 @@
 //counter code
-var button=document.getElementById('counter');
 
-button.onclick=function(){
-	//create the response and store it in avariable
-	var request = new XMLHttpRequest();
-
-	//capture the response and store it in a variable
-	request.onreadystatechange=function(){
-		if(request.readyState===XMLHttpRequest.DONE){
-			//take some action
-			if(request.status===200){
-				var counter=request.responseText;
-				var span=document.getElementById('count');
-				span.innerHTML=counter.toString(); 
-			}
-		}
-	}; 
-	//Make the request
-	request.open('GET','http://alibasit78.imad.hasura-app.io/counter',true);
-	request.send(null);
-
-};
-
-//submit name
+//submit username/password to login 
 
 var submit=document.getElementById("submit_btn");
 submit.onclick=function(){
@@ -37,7 +15,7 @@ submit.onclick=function(){
 			//take some action
 			if(request.status===200){
 				var names=request.responseText;
-					names=JSON.parse(names);
+				names=JSON.parse(names);
 				var list='';
 				for(var i=0;i<names.length;i++)
 				{
@@ -49,10 +27,13 @@ submit.onclick=function(){
 		}
 	}; 
 	//Make the request
-	var nameInput=document.getElementById('name');
-	var name=nameInput.value;
-	request.open('GET','http://alibasit78.imad.hasura-app.io/submit-name?name='+name,true);
-	request.send(null);
+	var username=document.getElementById('username').value;
+	var password=document.getElementById('password').value;
+	console.log(username);
+	console.log(password);
+	request.open('POST','http://alibasit78.imad.hasura-app.io/login',true);
+	request.setRequestHeader('Content-Type','application/json');
+	request.send(JSON.stringify({username:username,password:password}));
 	
 } 
 
