66942c70aef880353b5777e94d354fe671051e82
alibasit78 <alibasit78@gmail.com>
2017-09-03 12:29:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,20 +11,17 @@ submit.onclick=function(){
 
 	//capture the response and store it in a variable
 	request.onreadystatechange=function(){
-		if(request.readyState===XMLHttpRequest.DONE){
+	if(request.readyState===XMLHttpRequest.DONE){
 			//take some action
 			if(request.status===200){
-				var names=request.responseText;
-				names=JSON.parse(names);
-				var list='';
-				for(var i=0;i<names.length;i++)
-				{
-					list+='<li>' + names[i] + '</li>';
-				}
-				var ul=document.getElementById('namelist');
-				ul.innerHTML=list;
+				alert('logged in successfully');
+			} else if(request.status===403){
+				alert('Username/password is incorrect');
+			} else if(request.status===500){
+				alert('Something went wrong on the server');
 			}
 		}
+		
 	}; 
 	//Make the request
 	var username=document.getElementById('username').value;
